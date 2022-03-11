36e3ac6a6c69a73ffe6e9955f8b7e5a72447eb60
Aymane El-yamoun <ael-yamo@e1r10p3.1337.ma>
2022-03-11 18:02:38 +0100

    finshed & checked

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: ael-yamo <ael-yamo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 12:01:15 by ael-yamo          #+#    #+#             */
-/*   Updated: 2022/03/11 12:02:20 by ael-yamo         ###   ########.fr       */
+/*   Updated: 2022/03/11 17:59:33 by ael-yamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,27 @@ void	checker(t_list **stack_a, t_list **stack_b, int *i)
 		stat = aplly_op(op, stack_a, stack_b);
 		if (stat == 0)
 		{
-			write(2, "error\n", 6);
+			write(2, "Error\n", 6);
 			*i = 1;
 			break ;
 		}
 	}
+}
+
+int	main_utils(int argc, char **argv, t_list **stack_a)
+{
+	if (argc > 1)
+	{
+		if (check(stack_a, argc, argv) == 1)
+		{
+			write(2, "Error\n", 6);
+			free_stack(stack_a);
+			return (0);
+		}
+		return (1);
+	}
+	else
+		return (0);
 }
 
 int	main(int argc, char **argv)
@@ -83,22 +99,16 @@ int	main(int argc, char **argv)
 	int		i;
 
 	i = 0;
-	if (argc > 1)
+	if (main_utils(argc, argv, &stack_a))
 	{
-		if (check(&stack_a, argc, argv) == 1)
+		checker(&stack_a, &stack_b, &i);
+		if (i != 1)
 		{
-			write(2, "Error\n", 6);
-			free_stack(&stack_a);
-			return (0);
+			if (stack_is_sorted(&stack_a))
+				write(1, "======> OK <======\n", 19);
+			else
+				write(1, "======> KO <======\n", 19);
 		}
-	}
-	checker(&stack_a, &stack_b, &i);
-	if (i != 1)
-	{
-		if (stack_is_sorted(&stack_a))
-			write(1, "======> OK <======\n", 19);
-		else
-			write(1, "======> KO <======\n", 19);
 	}
 	return (0);
 }
